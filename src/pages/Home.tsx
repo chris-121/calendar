f31f7a44324579b56fcfc2a3b7869d15db9837e4
asitmandal <84184502+Asit-website@users.noreply.github.com>
2022-02-03 23:30:48 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Features from "./Features";
 import Hero from "./Hero";
+import Footer from "../Components/Footer"
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Features />
+      <Footer/>
     </div>
   );
 };
